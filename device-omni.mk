cffb66bc3b8ae868704dd851ffb0599bb94ed0e1
Chirayu Desai <chirayudesai1@gmail.com>
2022-03-12 04:11:07 +0530

    Build EuiccSupportPixelOverlay
    Change-Id: I46db4122b89798457338f6751982865571529ba6

--- device-omni.mk
+++ device-omni.mk
@@ -6,6 +6,9 @@ PRODUCT_COPY_FILES += \
     device/google/pantah/permissions/permissions_com.google.android.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/permissions_com.google.android.euicc.xml \
     device/google/pantah/permissions/permissions_com.google.euiccpixel.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/permissions_com.google.euiccpixel.xml
 
+PRODUCT_PACKAGES += \
+    EuiccSupportPixelOverlay
+
 # For Google Camera
 PRODUCT_COPY_FILES += \
     device/google/pantah/the_experiences.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/the_experiences.xml
