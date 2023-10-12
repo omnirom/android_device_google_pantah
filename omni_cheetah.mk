763e709be3344f23e3b1c730545f590c4ea41767
Chirayu Desai <chirayudesai1@gmail.com>
2023-10-11 20:48:48 -0500

    Init omnirom makefiles

---
+++ omni_cheetah.mk
@@ -0,0 +1,20 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/gs201/omni_common.mk)
+$(call inherit-product, device/google/pantah/device-omni.mk)
+$(call inherit-product, device/google/pantah/aosp_cheetah.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := omni_cheetah
+PRODUCT_MODEL := Pixel 7 Pro
+PRODUCT_BRAND := google
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=cheetah \
+    PRIVATE_BUILD_DESC="cheetah-user 14 UP1A.231005.007 10754064 release-keys"
+
+BUILD_FINGERPRINT := google/cheetah/cheetah:14/UP1A.231005.007/10754064:user/release-keys
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
